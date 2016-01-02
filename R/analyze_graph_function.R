37813620a44d01b3e57f64e9b9a7918c81f848fe
Doug Needham <sheridan3003@yahoo.com>
2016-01-02 08:28:40 -0500

    Return an object instead of global variables.

--- R/analyze_graph_function.R
+++ R/analyze_graph_function.R
@@ -1,9 +1,6 @@
-#gs.graph <- data.frame()
-#gs.node <- data.frame()
-#gs.edge <- data.frame()
-
+library(igraph)
 analyze.graph <- function(reference.graph) {
-  gs.node <<- data.frame(node = unlist(list(V(reference.graph)$name))
+  node <- data.frame(node = unlist(list(V(reference.graph)$name))
                          ,In = unlist(list(degree(reference.graph,mode="in")))
                          ,Out = unlist(list(degree(reference.graph,mode="out")))
                          ,Degree = unlist(list(degree(reference.graph,mode="total")))
@@ -12,21 +9,22 @@ analyze.graph <- function(reference.graph) {
                          ,Betweenness = unlist(list(betweenness(reference.graph)))
                          ,PageRank = unlist(list(page.rank(reference.graph)$vector))
                          )
-  gs.graph <<- data.frame(Nodes = length(V(reference.graph))
+  graph <- data.frame(Nodes = length(V(reference.graph))
                           ,edges = length(E(reference.graph))
-                          ,AverageDegree = mean(gs.node$Degree)
+                          ,AverageDegree = mean(node$Degree)
                           ,Density = 2*length(E(reference.graph))/(length(V(reference.graph))*length(V(reference.graph))-length(V(reference.graph)))
                           ,Diameter = diameter(reference.graph)
                           )
   if(is.directed(reference.graph)) { type = 'Directed'} else { type = 'Undirected'}
   if(is.null(E(reference.graph)$weight)) { E(reference.graph)$weight <- rep(1,length(E(reference.graph)))}
-  gs.edge <<- data.frame (EdgeLabel = get.edgelist(reference.graph)
+  edge <- data.frame (EdgeLabel = get.edgelist(reference.graph)
                           ,Type= type
                           ,Weight = E(reference.graph)$weight)
 
-  names(gs.edge) <<- c("Source","Target","Type","Weight")
+  names(edge) <- c("Source","Target","Type","Weight")
 
+  gs <- list(Node=node,Edge=edge,Graph=graph)
 
-  print(gs.graph)
+  return(gs)
 
 }
